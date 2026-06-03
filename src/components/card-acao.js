9ab19c695b9a661402725e13350a1cff31d9c431
Moisés Emanuel <moisesemanuelreisdacruz@hotmail.com>
2026-06-03 12:25:47 -0300

    fix: retirando componente e estilos inutilizados

--- src/components/card-acao.js
+++
@@ -1,9 +0,0 @@
-export function cardAcao({ href, icone, titulo, descricao }) {
-    return `
-        <a href="${href}" class="card card-action">
-            <i data-lucide="${icone}" class="icon-lg"></i>
-            <h3>${titulo}</h3>
-            <p>${descricao}</p>
-        </a>
-    `;
-}
