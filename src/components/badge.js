ff5b692910f9b7a70eae3787f1e7bb0368ba74a3
Moisés Emanuel <moisesemanuelreisdacruz@hotmail.com>
2026-06-03 16:20:17 -0300

    style: substituindo ícone por texto

--- src/components/badge.js
+++ src/components/badge.js
@@ -1,11 +1,12 @@
 export function badge({ status }) {
     const isResolvida = status === "Resolvida";
     const badgeClass = isResolvida ? "badge-success" : "badge-warning";
-    const badgeIcon = isResolvida ? "check-circle" : "circle-question-mark";
 
     return `
         <span class="badge ${badgeClass}">
-            <i data-lucide="${badgeIcon}"></i>
+            <span>
+                ${status}
+            </span>
         </span>
     `;
 }
